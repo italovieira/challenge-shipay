d7275f8e21611caeefbe42cbec7f2a3c157b896c
Ítalo Vieira <italogv@hotmail.com>
2020-08-16 12:16:19 -0300

    Setup database with SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from config import config
 from .api import configure_api
+from .models import configure as configure_db
 
 
 def create_app(config_name):
     app = Flask(__name__)
 
     app.config.from_object(config[config_name])
+
+    configure_db(app)
 
     configure_api(app)
 
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from os import getenv
 class Config:
     APP_PORT = int(getenv('APP_PORT'))
     DEBUG = eval(getenv('DEBUG').title())
+    SQLALCHEMY_DATABASE_URI = getenv('DB_URI')
 
 class DevelopmentConfig(Config):
     FLASK_ENV = 'development'
