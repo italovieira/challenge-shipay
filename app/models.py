bdb0324b3d4aaf152ee21a504dd24f485c24cd70
Ítalo Vieira <italogv@hotmail.com>
2020-08-16 21:32:25 -0300

    Add models

--- app/models.py
+++ app/models.py
@@ -7,3 +7,21 @@ def configure(app):
 
     with app.app_context():
         db.create_all()
+
+
+class Store(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cnpj = db.Column(db.String(14), unique=True, nullable=False)
+    transactions = db.relationship('Transaction', back_populates='store')
+
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cpf = db.Column(db.String(12), unique=True, nullable=False)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    store_id = db.Column(db.Integer, db.ForeignKey('store.id'))
+    store = db.relationship('Store', back_populates='transactions')
+    client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
+    client = db.relationship('Client')
+    description = db.Column(db.String(120))
